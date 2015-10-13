8baecdc44a9d2e497d5e4431c86b5e4e550c4747
Jon Szymaniak <jon.szymaniak@nuand.com>
2015-10-13 11:23:10 -0400

    class: [WIP] Whitespace cleanup

--- class/bladeRF.m
+++ class/bladeRF.m
@@ -4,16 +4,16 @@ classdef bladeRF < handle
         status  % Device status of last call
         device  % Device handle
     end
-    
+
     properties
         rx      % Receive chain
         tx      % Transmit chain
     end
-    
+
     properties(SetAccess=immutable)
         versions
     end
-    
+
     methods(Hidden)
         function check(obj, name)
             if obj.status ~= 0
@@ -22,12 +22,12 @@ classdef bladeRF < handle
                 throw(MException(msgID, msg)) ;
             end
         end
-        
+
         function set_status(obj, status)
             obj.status = status ;
         end
     end
-    
+
     methods(Static, Access = private)
         function load_library
             % Load the library
@@ -48,14 +48,14 @@ classdef bladeRF < handle
                 if isempty(notfound) == false
                     error('bladeRF:loadlibrary', 'functions missing from library' ) ;
                 end
-                
+
                 if isempty(warnings) == false
                     warning('bladeRF:loadlibrary', 'loadlibrary returned warning messages \n%s\n', warnings) ;
                 end
             end
         end
     end
-    
+
     methods(Static)
         function devs = devices
             bladeRF.load_library() ;
@@ -77,12 +77,12 @@ classdef bladeRF < handle
             calllib('libbladeRF', 'bladerf_free_device_list', pdevlist) ;
         end
     end
-    
+
     methods
         % Constructor
         function obj = bladeRF(devstring)
             bladeRF.load_library() ;
-            
+
             % Populate version information
             ver = libstruct('bladerf_version');
             ver.major = 0;
@@ -98,21 +98,22 @@ classdef bladeRF < handle
             obj.versions.matlab.major = 1 ;
             obj.versions.matlab.minor = 0 ;
             obj.versions.matlab.patch = 0 ;
-            
+
             % Open the instance
             dptr = libpointer('bladerfPtr') ;
             obj.status = calllib('libbladeRF', 'bladerf_open',dptr, devstring) ;
-            
+
             % Check the return value
             obj.check('bladeRF_open') ;
-            
+
             % Save off the device pointer
             obj.device = dptr ;
-            
+
             % Create the device transceiver chain
             obj.rx = XCVR(obj, 'RX') ;
             obj.tx = XCVR(obj, 'TX') ;
         end
+
 
         % Destructor
         function delete(obj)
@@ -126,14 +127,14 @@ classdef bladeRF < handle
             disp('Sending something') ;
             % device.send(x) ;
         end
-        
+
         % RX samples immediately
         function ret = receive(obj, n)
             % Receive something
             disp('Receiving something') ;
             % ret = device.receive(x) ;
         end
-        
+
         % Low level peek function
         function val = peek(obj, dev, addr)
             switch dev
@@ -142,13 +143,13 @@ classdef bladeRF < handle
                     [obj.status,~,x]  = calllib('libbladeRF', 'bladerf_dac_read', obj.device, x) ;
                     obj.check('bladerf_dac_read') ;
                     val = x ;
-                    
+
                 case 'lms'
                     x = uint8(0) ;
                     [obj.status,~,x] = calllib('libbladeRF', 'bladerf_lms_read', obj.device, addr, x) ;
                     obj.check('bladerf_lms_read') ;
                     val = x ;
-                    
+
                 case 'si'
                     x = uint8(0) ;
                     [obj.status,~,x] = calllib('libbladeRF', 'bladerf_si5338_read', obj.device, addr, x) ;
@@ -157,29 +158,29 @@ classdef bladeRF < handle
 
             end
         end
-        
+
         % Low level poke function
         function poke(obj, dev, addr, val)
             switch dev
                 case 'dac'
                     obj.status = calllib('libbladeRF', 'bladerf_dac_write', obj.device, val) ;
                     obj.check('bladerf_dac_write') ;
-                    
+
                 case 'lms'
                     obj.status = calllib('libbladeRF', 'bladerf_lms_write', obj.device, addr, val) ;
                     obj.check('bladerf_lms_write') ;
-                    
+
                 case 'si'
                     obj.status = calllib('libbladeRF', 'bladerf_si5338_write', obj.device, addr, val) ;
                     bladeRF.check('bladerf_si5338_write') ;
             end
         end
-        
+
         % Load the FPGA from MATLAB
         function load_fpga(obj, filename)
             rv = calllib('libbladeRF', 'bladerf_load_fpga', obj.device, filename) ;
             bladeRF.check('bladerf_load_fpga', rv) ;
         end
-        
+
     end
 end
